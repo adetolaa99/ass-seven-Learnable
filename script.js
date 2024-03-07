148ae5c844d313136f1343383c6bdf9f044e95ee
Nofisat Hassan <hassannofisat017@gmail.com>
2024-03-07 20:16:18 +0100

    first

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ function addTodo() {
   todoInput.value = todoText;
   todoInput.readOnly = true;
 
+  const editButton = document.createElement("button");
+  editButton.innerText = "Edit";
+  editButton.onclick = function () {
+    todoInput.readOnly = false;
+    todoInput.focus();
+  };
+
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "Delete";
   deleteButton.onclick = function () {
@@ -19,6 +26,7 @@ function addTodo() {
   };
 
   listItem.appendChild(todoInput);
+  listItem.appendChild(editButton);
   listItem.appendChild(deleteButton);
 
   todoList.appendChild(listItem);
